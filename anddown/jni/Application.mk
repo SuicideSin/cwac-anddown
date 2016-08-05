894971bf36ae42b09d8d9127099ecea906003b2b
n8fr8 <nathan@freitas.net>
2016-08-05 14:33:03 -0400

    remove 64 bit native targets so this can work with SQLCipher

--- anddown/jni/Application.mk
+++ anddown/jni/Application.mk
@@ -1,2 +1,3 @@
-APP_ABI := armeabi armeabi-v7a x86 x86_64 arm64-v8a
+APP_ABI := armeabi armeabi-v7a x86 #remove 64 bit support so it works with SQLCipher
+#APP_ABI := armeabi armeabi-v7a x86 x86_64 arm64-v8a
 # APP_ABI := all
